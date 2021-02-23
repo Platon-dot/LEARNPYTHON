a65a753bd5337b87856c64837decb09114dc6dc4
UncleStifler <ambalexey@gmail.com>
2021-02-23 14:40:02 +0100

    time_date

--- lesson3/datetime_work.py
+++ lesson3/datetime_work.py
@@ -18,11 +18,30 @@ print(dt3)
 dt = date(2000, 1, 1)
 print(dt)
 delta = timedelta(days=1)  # создали дельту в которой лежит 1 день
-print(delta)
+print(delta)  # выводим на печать то что положили в delta
 # теперь математические действия
 print(dt - delta)
 print(dt + delta)
 
-# вывод даты на экран
-dt.now.strflime("%d.%m.%Y %H:%m")
-dt.now.strflime("%d-%m-%Y")
+# вывод даты на экран в формате strftime
+dt_now.strftime("%d.%m.%Y %H:%m")
+print(dt_now.strftime("%d.%m.%Y %H:%m"))
+
+dt_now.strftime("%d-%m-%Y")
+print(dt_now.strftime("%d-%m-%Y"))
+
+dt_now.strftime("%A %d %B %Y")
+print(dt_now.strftime("%A %d %B %Y"))
+
+# вывод формата даты на локальных языках
+
+import locale
+locale.setlocale(locale.LC_TIME, "es_ES")
+print(dt_now.strftime("%A %d %B %Y"))
+
+
+# Получение даты из строки
+
+date_string = '12/23/2010'  # получаем дату в виде строки
+date_dt = datetime.strptime(date_string, '%m/%d/%Y') # кормим ему переменную со строкой и формат в ктором я хочу это увидеть
+print(date_dt)
