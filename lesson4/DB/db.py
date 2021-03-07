4b840eb35dc4ec43e5143b1e930d809ec53ce5f4
UncleStifler <ambalexey@gmail.com>
2021-03-08 00:22:27 +0100

    update ignore and delete link

--- lesson4/DB/db.py
+++ lesson4/DB/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker # общение с базой данных
+import settings
 
-engine = create_engine('postgres://bhzqldql:ilkNeRXrUCQGbvUl_LHN_ZhcXfEDlu4Y@hattie.db.elephantsql.com:5432/bhzqldql') # https://api.elephantsql.com/
+engine = create_engine(settings.DB_LINK) # https://api.elephantsql.com/
 db_session = scoped_session(sessionmaker(bind=engine)) # db_session - позволяет отправлять запросы в базу данных
 
 Base = declarative_base()
